736c636241dcccd492b7547f742948161b466b0f
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-03 19:52:21 -0400

    tab limit per window 10

--- popup.js
+++ popup.js
@@ -2,12 +2,27 @@
 chrome.tabs.query({}, function(tabs) {
   var count = tabs.length;
 
-  // If there are more than 10 tabs, close the current tab
-  if (count > 10) {
-    chrome.tabs.getCurrent(function(tab) {
+  const MAX_TABS = 10;
+
+// Listen for new tab created
+chrome.tabs.onCreated.addListener(function(tab) {
+  // Get all tabs in the current window
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+    // Check if the number of tabs exceeds the maximum
+    if (tabs.length > MAX_TABS) {
+      // Close the newly created tab
       chrome.tabs.remove(tab.id);
-    });
-  }
+      // Display an error message to the user
+      chrome.notifications.create({
+        type: "basic",
+        title: "Tab Limit Exceeded",
+        message: "You have reached the maximum number of tabs in this window.",
+        iconUrl: "icon-48.png"
+      });
+    }
+  });
+});
+
 
   // Update the tab counter in the popup
   document.getElementById("tab-counter").textContent = count;
