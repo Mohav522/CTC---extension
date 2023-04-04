f3e16cf7d438742a91016caeac93c4a51bb9455f
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-03 20:22:34 -0400

    update

--- popup.js
+++ popup.js
@@ -1,18 +1,16 @@
-// Count the number of tabs
 chrome.tabs.query({}, function(tabs) {
   var count = tabs.length;
 
   const MAX_TABS = 10;
 
-// Listen for new tab created
 chrome.tabs.onCreated.addListener(function(tab) {
-  // Get all tabs in the current window
+  
   chrome.tabs.query({currentWindow: true}, function(tabs) {
-    // Check if the number of tabs exceeds the maximum
+    
     if (tabs.length > MAX_TABS) {
-      // Close the newly created tab
+     
       chrome.tabs.remove(tab.id);
-      // Display an error message to the user
+      
       chrome.notifications.create({
         type: "basic",
         title: "Tab Limit Exceeded",
@@ -23,7 +21,5 @@ chrome.tabs.onCreated.addListener(function(tab) {
   });
 });
 
-
-  // Update the tab counter in the popup
   document.getElementById("tab-counter").textContent = count;
 });
