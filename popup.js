11f8e058ccb2b5907527a4dbec89439b11c3af8d
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-03 20:46:10 -0400

    alert notification

--- popup.js
+++ popup.js
@@ -10,9 +10,6 @@ chrome.tabs.onCreated.addListener(function(tab) {
     
     if (tabs.length > MAX_TABS) {
       console.log("Too many tabs!");
-        alert("Tab limit of 10 exceeded!");
-        chrome.tabs.remove(tabs[0].id);
-      
       chrome.notifications.create({
         type: "basic",
         title: "Tab Limit Exceeded",
