6e445e7ac078bc06fc67b9e0a233db8ed9239753
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-20 12:37:54 -0400

    Update content.js

--- content.js
+++ content.js
@@ -1,12 +1,11 @@
 
-  // Get the current tab count
-let tabCount = window.top.document.querySelectorAll("div[role='tab']").length;
-
-// If the tab count is greater than 10, prevent new tabs from opening
-if (tabCount >= 10) {
-  window.top.document.body.addEventListener("click", function(event) {
-    if (event.target.matches("a") && event.button === 0) {
-      event.preventDefault();
-    }
-  }, true);
-}
+// Listen for the onCreated event, which is fired when a new tab is created.
+chrome.tabs.onCreated.addListener(function(tab) {
+    // Get all tabs in the current window.
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+      // If the number of tabs in the window is greater than 10, close the new tab.
+      if (tabs.length > 10) {
+        chrome.tabs.remove(tab.id);
+      }
+    });
+  });
